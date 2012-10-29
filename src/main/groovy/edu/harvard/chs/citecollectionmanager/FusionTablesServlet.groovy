80203e775e2fc311166133ea0c9ce358ca76d51b
Ryan Baumann <ryan.baumann@gmail.com>
2012-10-29 15:26:43 -0400

    Hook up first pass at working /tables/ request parodying.

--- src/main/groovy/edu/harvard/chs/citecollectionmanager/FusionTablesServlet.groovy
+++ src/main/groovy/edu/harvard/chs/citecollectionmanager/FusionTablesServlet.groovy
@@ -1,5 +1,12 @@
 package edu.harvard.chs.citecollectionmanager
 
+import com.google.api.client.auth.oauth2.Credential
+import com.google.api.services.fusiontables.Fusiontables
+import com.google.api.services.fusiontables.Fusiontables.Builder
+import com.google.api.services.fusiontables.model.Table
+import com.google.api.services.fusiontables.model.TableList
+import com.google.api.services.fusiontables.model.Sqlresponse
+
 import java.io.*
 import java.net.URL
 import java.util.*
@@ -16,11 +23,16 @@ public class FusionTablesServlet extends HttpServlet {
   protected void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException
   {
-    CodeFlow.instance.build()
+    Credential credential = CodeFlow.instance.build().loadCredential('administrator')
+    String table_id = request.getRequestURL().toString().split("/").last()
+    Fusiontables fusiontables = new Fusiontables.Builder(CodeFlow.HTTP_TRANSPORT, CodeFlow.JSON_FACTORY, credential).build()
+
+    Table table = fusiontables.table().get(table_id).execute()
+
     response.setContentType("text/html;charset=UTF-8")
     response.setStatus(HttpServletResponse.SC_OK)
     PrintWriter out = response.getWriter()
 
-    out.println("ok")
+    out.println(table.toPrettyString())
   }
 }
