20132bd216154bd63216cf3dc7c38e893f5f12a5
Ryan Baumann <ryan.baumann@gmail.com>
2012-10-30 14:55:56 -0400

    Return forbidden responses if user is blocked in the authorization table, and do the same in the tables proxy servlet.

--- src/main/groovy/edu/harvard/chs/citecollectionmanager/FusionTablesServlet.groovy
+++ src/main/groovy/edu/harvard/chs/citecollectionmanager/FusionTablesServlet.groovy
@@ -6,6 +6,12 @@ import com.google.api.services.fusiontables.Fusiontables.Builder
 import com.google.api.services.fusiontables.model.Table
 import com.google.api.services.fusiontables.model.TableList
 import com.google.api.services.fusiontables.model.Sqlresponse
+import com.google.api.services.oauth2.Oauth2
+import com.google.api.services.oauth2.Oauth2.Builder
+import com.google.api.services.oauth2.model.Tokeninfo
+import com.google.api.services.oauth2.model.Userinfo
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential.Builder
 
 import java.io.*
 import java.net.URL
@@ -14,6 +20,7 @@ import javax.servlet.*
 import javax.servlet.http.*
 
 import edu.harvard.chs.citecollectionmanager.CodeFlow
+import edu.harvard.chs.citecollectionmanager.UserAuthorization
 
 public class FusionTablesServlet extends HttpServlet {
   /**
@@ -23,16 +30,28 @@ public class FusionTablesServlet extends HttpServlet {
   protected void doGet(HttpServletRequest request, HttpServletResponse response)
     throws ServletException, IOException
   {
-    Credential credential = CodeFlow.instance.build().loadCredential('administrator')
-    String table_id = request.getRequestURL().toString().split("/").last()
-    Fusiontables fusiontables = new Fusiontables.Builder(CodeFlow.HTTP_TRANSPORT, CodeFlow.JSON_FACTORY, credential).build()
+    String user_access_token = request.getParameter("access_token")
+    GoogleCredential google_credential = new GoogleCredential().setAccessToken(user_access_token)
+    Oauth2 oauth2 = new Oauth2.Builder(CodeFlow.HTTP_TRANSPORT, CodeFlow.JSON_FACTORY, google_credential).build()
+    Userinfo userinfo = oauth2.userinfo().get().execute()
+    
+    UserAuthorization user_authorization = new UserAuthorization(userinfo)
 
-    Table table = fusiontables.table().get(table_id).execute()
+    if(user_authorization.authorized()) {
+      Credential credential = CodeFlow.instance.build().loadCredential('administrator')
+      String table_id = request.getRequestURL().toString().split("/").last()
+      Fusiontables fusiontables = new Fusiontables.Builder(CodeFlow.HTTP_TRANSPORT, CodeFlow.JSON_FACTORY, credential).build()
 
-    response.setContentType("text/html;charset=UTF-8")
-    response.setStatus(HttpServletResponse.SC_OK)
-    PrintWriter out = response.getWriter()
+      Table table = fusiontables.table().get(table_id).execute()
 
-    out.println(table.toPrettyString())
+      response.setContentType("text/html;charset=UTF-8")
+      response.setStatus(HttpServletResponse.SC_OK)
+      PrintWriter out = response.getWriter()
+
+      out.println(table.toPrettyString())
+    }
+    else {
+      response.setStatus(HttpServletResponse.SC_FORBIDDEN)
+    }
   }
 }
